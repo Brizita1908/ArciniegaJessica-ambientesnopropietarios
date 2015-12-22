e2e7bc577548d4e0dae4fac65ae487709fe0c9f6
Jessica <brigatita1795@hotmail.com>
2015-12-22 12:46:59 -0500

    deber1bimestre2

---
+++ bimestre2/deberes/deber1/index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Homepage</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="menu">
+        <nav class="row navbar navbar-default">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="col-md-offset-1 col-md-3 navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="col-md-3 navbar-brand" href="#">
+                    <img src="img/logo.png">
+                  </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class="active">
+                      <a href="#">
+                        Inicio <span class="sr-only">(actual)</span>
+                      </a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Portafolio <span class="caret"></span>
+                      </a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Fotografias</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Wordpress</a></li>
+                        <li><a href="#">Javascript</a></li>
+                      </ul>
+                    </li>
+
+                    <li>
+                      <a href="#">
+                        Acerca de mi <span class="sr-only">(actual)</span>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="#">
+                        Contactame <span class="sr-only">(actual)</span>
+                      </a>
+                    </li>
+
+                    <li>
+                      <button class="btn-blog btn btn-default btn-sm">
+                        Blog <span class="sr-only">(actual)</span>
+                      </button>
+                    </li>
+
+                  </ul>
+                  <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn-search">
+                      <img src="img/search.png">
+                    </button>
+                  </form>
+                 
+                </div><!-- /.navbar-collapse -->
+            </nav>
+          </div>
+
+      <div class="row" id="header">
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="c-in carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="ind active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1" class="ind"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2" class="ind"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3" class="ind"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4" class="ind"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/fondo1.jpg" alt="...">
+      <div class="text-cap carousel-caption">
+        <h1>GOOD DESIGN IS GOOD BUSINESS</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices sagittis magnaa commodo.<br>
+         Ut eget eros mauris, ut cursus urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+        <button class="btn-learn btn btn-default btn-sm">LEARN MORE</button>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/fondo2.jpg" alt="...">
+      <div class="text-cap carousel-caption">
+        <h1>GOOD DESIGN IS GOOD BUSINESS</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices sagittis magnaa commodo.<br>
+         Ut eget eros mauris, ut cursus urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+        <button class="btn-learn btn btn-default btn-sm">LEARN MORE</button>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/fondo3.jpg" alt="...">
+      <div class="text-cap carousel-caption">
+        <h1>GOOD DESIGN IS GOOD BUSINESS</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices sagittis magnaa commodo.<br>
+         Ut eget eros mauris, ut cursus urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+        <button class="btn-learn btn btn-default btn-sm">LEARN MORE</button>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/fondo4.jpg" alt="...">
+      <div class="text-cap carousel-caption">
+        <h1>GOOD DESIGN IS GOOD BUSINESS</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices sagittis magnaa commodo.<br>
+         Ut eget eros mauris, ut cursus urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+        <button class="btn-learn btn btn-default btn-sm">LEARN MORE</button>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/fondo5.jpg" alt="...">
+      <div class="text-cap carousel-caption">
+        <h1>GOOD DESIGN IS GOOD BUSINESS</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices sagittis magnaa commodo.<br>
+         Ut eget eros mauris, ut cursus urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+        <button class="btn-learn btn btn-default btn-sm">LEARN MORE</button>
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+      </div>
+      <div class="row" id="services">
+        <div class="col-md-12">
+          <div class="col-md-3">
+            <img src="img/laptop.png" class="img-s" alt="">
+            <h3>Web Design</h3>
+            <p>Lorem ipsum dolor sit amet,<br>
+              consectetur adipiscing elit.<br>
+              Mauris nec ipsum at lacus<br>
+              commodo susciplit. <a href="">Details...</a></p>
+          </div> 
+          <div class="col-md-3">
+            <img src="img/icon.png" class="img-s" alt="">
+            <h3>Search Optimization</h3>
+            <p>Lorem ipsum dolor sit amet,<br>
+              consectetur adipiscing elit.<br>
+              Mauris nec ipsum at lacus<br>
+              commodo susciplit. <a href="">Details...</a></p>
+          </div> 
+          <div class="col-md-3">
+            <img src="img/carro.png" class="img-s" alt="">
+            <h3>Online Shop</h3>
+            <p>Lorem ipsum dolor sit amet,<br>
+              consectetur adipiscing elit.<br>
+              Mauris nec ipsum at lacus<br>
+              commodo susciplit. <a href="">Details...</a></p>
+          </div> 
+          <div class="col-md-3">
+            <img src="img/like.png" class="img-s" alt="">
+            <h3>Social Media</h3>
+            <p>Lorem ipsum dolor sit amet,<br>
+              consectetur adipiscing elit.<br>
+              Mauris nec ipsum at lacus<br>
+              commodo susciplit. <a href="">Details...</a></p>
+          </div> 
+        </div>        
+      </div>
+      <div class="row" id="portfolio">
+        <div class="col-md-12">
+          <h2 class="titulo txt">Our Portfolio Awesome <button class="btn-p btn btn-default btn-sm">More Works</button></h2>
+          <div class="row" id="images">
+            <div class="col-md-12">
+              <div class="col-md-4">
+                <img src="img/image1.png" class="img-p" align="">
+                <h3 class="txt">Lorem Ipsum Dolar Sit Amet</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+                   Mauris nec ipsum at lacus commodo susciplit.</p>
+              </div> 
+              <div class="col-md-4">
+                <img src="img/image1.png" class="img-p" align="">
+                <h3 class="txt">Lorem Ipsum</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscinglit.</p>
+              </div> 
+              <div class="col-md-4">
+                <img src="img/image2.png" class="img-p" align="">
+                <h3 class="txt">Ipsum Amet Dolar Sit</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing <br>
+                  elitmauris nec ipsum at lacus commodo susciplit.</p>
+              </div> 
+            </div>        
+          </div>
+        </div>        
+      </div> 
+      <div class="row" id="offer">
+        <div class="col-md-12">
+          <h2>WHAT WE OFFER</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices sagittis magnaa commodo. Ut eget eros mauris, ut cursus urna.<br>
+             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.</p>
+          <div class="row" id="text">
+            <div class="text col-md-12">
+              <div class="col-md-3">
+                <h3>Web Design</h3>
+                <p>Lorem ipsum dolor sit amet, consect
+                  etur adipiscing elit. Ut ultrices sagit 
+                  tism agnaa commodo. Ut eget eros
+                  mauris, ut cursus urna vestibuluman
+                  ipsum primis in faucibus orci luctuset.</p>
+                  <button class="btn-details btn btn-default btn-sm">Details</button>
+              </div> 
+              <div class="col-md-3">
+                <h3>Search Optimization</h3>
+                <p>Lorem ipsum dolor sit amet, consect
+                  etur adipiscing elit. Ut ultrices sagit 
+                  tism agnaa commodo. Ut eget eros
+                  mauris, ut cursus urna vestibuluman
+                  ipsum primis in faucibus <a href="">Details...</a></p>
+              </div> 
+              <div class="col-md-3">
+                <h3>Online Shop</h3>
+                <p>Lorem ipsum dolor sit amet, consect
+                  etur adipiscing elit. Ut ultrices sagit 
+                  tism agnaa commodo. Ut eget eros
+                  mauris, ut cursus urna vestibuluman
+                  ipsum primis in faucibus orci luctuset.</p>
+                  <button class="btn-details btn btn-default btn-sm">Details</button>
+              </div> 
+              <div class="col-md-3">
+                <h3>Social Media</h3>
+                <p>Lorem ipsum dolor sit amet, consect
+                  etur adipiscing elit. Ut ultrices sagit 
+                  tism agnaa commodo. Ut eget eros
+                  mauris, ut cursus urna vestibuluman
+                  ipsum primis in faucibus orci luc</p>
+              </div> 
+            </div>        
+          </div>
+          <hr>
+          <h2>TESTIMONIALS</h2>
+          <h4><b>"Lorem ipsum dolor sit amet, consect etur adipiscing elit mauris necip<br>
+             at lacus commodo suscipit praesent sollicitudin enim vel mirhon"</b></h4>
+
+          <p id="jon"><b>Jon Doe</b> / CEO of Loremipsum</p>
+          <img src="img/mujer.png" class="img-t" alt="">
+          <img src="img/hombre.png" class="img-t" alt="">
+          <img src="img/mujer.png" class="img-t" alt="">
+        </div>        
+      </div>
+      <div class="row" id="footer">
+        <div class="col-md-12">
+          <div class="col-md-3">
+            <h3 class="txt">TWITTER FEEDS</h3>
+            <p>Check out this great theme item
+              <a href="">http:tadjalskfj.com//</a>
+              <br>2 weeks ago</p>
+            <hr>
+            <p>Check out this great theme item for
+              <a href="">http:tadjalskfj.com//</a>
+              <br>2 weeks ago</p>
+            <hr>
+            <p>Check out this great theme item for you
+              <a href="">http:tadjalskfj.com//</a>
+              <br>2 weeks ago</p>
+          </div> 
+          <div class="col-md-3">
+            <h3 class="txt">NEWSLETTER</h3>
+            <p>Lorem ipsum dolor sit amet,
+              iscing elit. Ut ultrices magna a comm.
+              Ut eget eros mauris, ut cursus ultrices urna.</p>
+            <hr>
+            <p id="email">Your Email Address</p>
+            <input type="text" class="in-email input"></input>
+            <button class="btn-signup btn btn-default btn-sm">Sign Up</button>
+          </div> 
+          <div class="col-md-3">
+            <h3 class="txt">RECENT PROJECTS</h3>
+              <p><img src="img/rectangle.png" class="img-f" align="left">
+              Sketchy Bussiness Card
+              <a href="">http:tadjalskfj.com//</a>
+              <br>2 weeks ago</p>
+              <hr>
+              <p><img src="img/rectangle.png" class="img-f" align="left">Message Boards in Nature
+              <a href="">http:tadjalskfj.com//</a>
+              <br>2 weeks ago</p>
+              <hr> 
+              <p>
+              <img src="img/rectangle.png" class="img-f" align="left">
+              Website Design Kit
+              <a href="">http:tadjalskfj.com//</a>
+              <br>2 weeks ago</p> 
+          </div> 
+          <div class="col-md-3">
+            <h3 class="txt">CONTACTS</h3>
+              <p><img src="img/telf.png" class="img-f" align="left">
+              1 - 000 - 000-0000<br>
+              1 - 000 - 000-0000</p>
+              <p><img src="img/cell.png" class="img-f" align="left">
+              1 - 000 - 000-0000<br>
+              1 - 000 - 000-0000</p>
+              <p><img src="img/mail.png" class="img-f" align="left">
+              abcdefg@hijs.dfg<br>
+              fjashfaf@jkfs.ckd</p> 
+          </div> 
+        </div>        
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
